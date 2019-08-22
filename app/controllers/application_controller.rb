cff0a29d5943cc55e03f037571cef1edfd95fef2
Irina Kagan <irinagamb@gmail.com>
2019-08-22 12:53:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+<<<<<<< HEAD
     user = User.find_by(username: params["username"], password: params["password"])
     if user
       session[:user_id] = user.id
@@ -22,11 +23,33 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     erb :account
+=======
+    @user = User.find_by(username: params["username"],password: params["password"])
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect '/account'
+  end
+  erb :error
+
+  end
+
+  get '/account' do
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
+>>>>>>> 9ce6aad1c57a5d20a4ca22b3bbe1117305dd3cfe
   end
 
   get '/logout' do
     session.clear
     redirect to '/'
+<<<<<<< HEAD
+=======
+
+>>>>>>> 9ce6aad1c57a5d20a4ca22b3bbe1117305dd3cfe
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,8 +3,19 @@ class Helpers
     @user = User.find(session_hash[:user_id])
   end
 
+<<<<<<< HEAD
   def self.is_logged_in?(session_hash)
    @res = !!session_hash[:user_id]
    @res
   end
+=======
+  def self.current_user(session)
+     User.find(session["user_id"])
+   end
+
+    def self.is_logged_in?(session)
+     !!session["user_id"]
+   end
+
+>>>>>>> 9ce6aad1c57a5d20a4ca22b3bbe1117305dd3cfe
 end
